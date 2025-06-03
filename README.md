663988caab677a9ec25a20ea7354920004863c0a
Yutong Zhang <zhangyt2000@gmail.com>
2025-06-03 17:12:26 -0600

    Add dependencies

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # h12_ros2_controller
+
 Controller of the h12 robot with ROS2 services
+
+## Installation
+
+- All the dependencies are listed in the `requirements.txt`\
+- Version numbers are determined by installing everything through pip in a separated conda environment defined in `environment.yml`.
+- Make sure you are using system-level python3, run `pip install -e .` to install all the dependencies.
+- Install the Unitree Python SDK from [here](https://github.com/unitreerobotics/unitree_sdk2_python/tree/master)
+  so the code can communicate with the robot.
+
+## Files
+
+TODO
--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ from setuptools import find_packages, setup
 
 package_name = 'h12_ros2_controller'
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name=package_name,
-    version='0.0.0',
+    name='h12_ros2_controller',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools'] + requirements,
     zip_safe=True,
     maintainer='tonyzyt2000',
     maintainer_email='zhangyt2000@gmail.com',
