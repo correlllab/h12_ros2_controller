fd3a9fbe7b62ff5bd31811a98136d7e402c9bf14
Yutong Zhang <zhangyt2000@gmail.com>
2025-06-13 13:47:57 -0600

    Fix path definition

--- h12_ros2_controller/utility/path_definition.py
+++ h12_ros2_controller/utility/path_definition.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 PACKAGE_PATH = get_package_share_directory('h12_ros2_controller')
 
 MODEL_PACKAGE_PATH = get_package_share_directory('h12_ros2_model')
-ASSETS_PATH = f'{MODEL_PACKAGE_PATH}/assets'
+MODEL_ASSETS_PATH = f'{MODEL_PACKAGE_PATH}/assets'
 # pinnochio query meshes relatively, ROS requires package:// prefix
-URDF_PIN_PATH = f'{MODEL_PACKAGE_PATH}/h1_2/h1_2.urdf'
-URDF_ROS_PATH = f'{MODEL_PACKAGE_PATH}/h1_2/h1_2_ros.urdf'
+URDF_PIN_PATH = f'{MODEL_ASSETS_PATH}/h1_2/h1_2.urdf'
+URDF_ROS_PATH = f'{MODEL_ASSETS_PATH}/h1_2/h1_2_ros.urdf'
