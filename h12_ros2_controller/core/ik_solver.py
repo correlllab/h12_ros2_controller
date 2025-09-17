c67adfbcea13e70b6d22ed1369538e375e70b808
Yutong Zhang <zhangyt2000@gmail.com>
2025-09-16 22:16:02 -0600

    Set larger acceleration limit and plot

--- h12_ros2_controller/core/ik_solver.py
+++ h12_ros2_controller/core/ik_solver.py
@@ -59,13 +59,13 @@ class IKSolver:
             pink.limits.ConfigurationLimit(robot_model.model),
             pink.limits.VelocityLimit(robot_model.model),
             pink.limits.AccelerationLimit(robot_model.model,
-                                          15.0 * np.ones(robot_model.model.nv))
+                                          25.0 * np.ones(robot_model.model.nv))
         ]
         self.limits_reduced = [
             pink.limits.ConfigurationLimit(robot_model.model_reduced),
             pink.limits.VelocityLimit(robot_model.model_reduced),
             pink.limits.AccelerationLimit(robot_model.model_reduced,
-                                          15.0 * np.ones(robot_model.model_reduced.nv))
+                                          25.0 * np.ones(robot_model.model_reduced.nv))
         ]
 
         # solver selection
