3f166e5fc974781e3217bbd4ae9b1ef78bda6bac
HumanoidDesktop <correlllabhumanoid@gmail.com>
2025-10-06 22:04:01 -0600

    New control scheme (dq=0) finally working on the real robot

--- h12_ros2_controller/plot/plot_control_record.py
+++ h12_ros2_controller/plot/plot_control_record.py
@@ -61,7 +61,7 @@ def plot_recording(filename, savepath):
         plt.close()
 
 if __name__ == '__main__':
-    path = 'record_mj'
+    path = 'record_real'
     filename = f'data/control_record/{path}.npz'
     savepath = f'figures/control_record/{path}'
     plot_recording(filename, savepath)
