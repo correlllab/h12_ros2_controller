5df1673a1eef13edf986fd2004f779f5075ae65c
Yutong Zhang <zhangyt2000@gmail.com>
2025-10-09 11:29:50 -0600

    Update view_urdf with command line argument

--- h12_ros2_controller/utility/view_urdf.py
+++ h12_ros2_controller/utility/view_urdf.py
@@ -1,17 +1,27 @@
+import os
+import argparse
 import pinocchio as pin
 from pinocchio.visualize import MeshcatVisualizer
 
-# URDF path
-urdf_path = 'assets/h1_2/h1_2_sphere.urdf'
-model_path = 'assets/h1_2'
+def view_urdf(filename):
+    # package dir as parent folder
+    model_path = os.path.dirname(os.path.abspath(filename))
 
-# load model
-model, visual, collision = pin.buildModelsFromUrdf(urdf_path,
-                                      package_dirs=model_path)
-viz = MeshcatVisualizer(model, visual, collision)
-viz.initViewer(open=True)
-viz.loadViewerModel()
+    # load model
+    model, visual, collision = pin.buildModelsFromUrdf(
+        filename,
+        package_dirs=model_path
+    )
+    viz = MeshcatVisualizer(model, visual, collision)
+    viz.initViewer(open=True)
+    viz.loadViewerModel()
 
-while True:
-    # display robot at neutral configuration
-    viz.display(pin.neutral(model))
+    while True:
+        # display robot at neutral configuration
+        viz.display(pin.neutral(model))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Visualize URDF model using Meshcat')
+    parser.add_argument('filename', type=str, help='URDF filename')
+    args = parser.parse_args()
+    view_urdf(args.filename)
