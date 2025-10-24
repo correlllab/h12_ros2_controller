0e8b85c89e122af4982a886b2fb9a9fa5097d957
Yutong Zhang <zhangyt2000@gmail.com>
2025-10-23 22:03:50 -0600

    Change file structure

--- setup.py
+++ setup.py
@@ -29,11 +29,11 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'joint_state_publisher = h12_ros2_controller.joint_state_publisher:main',
-            'dual_arm_server = h12_ros2_controller.dual_arm_server:main',
-            'dual_arm_client = h12_ros2_controller.dual_arm_client:main',
-            'hand_controller_node = h12_ros2_controller.hand_controller_node:main',
-            'hand_cmd_gui = h12_ros2_controller.hand_cmd_gui:main',
+            'joint_state_publisher = h12_ros2_controller.ros2.joint_state_publisher:main',
+            'dual_arm_server = h12_ros2_controller.ros2.dual_arm_server:main',
+            'dual_arm_client = h12_ros2_controller.ros2.dual_arm_client:main',
+            'hand_controller_node = h12_ros2_controller.ros2.hand_controller_node:main',
+            'hand_cmd_gui = h12_ros2_controller.ros2.hand_cmd_gui:main',
         ],
     },
 )
